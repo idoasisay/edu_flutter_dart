ec71525a812079c2b1236c94218820f955146691
idoasisay <ihannah@kakao.com>
2024-05-24 22:00:31 +0900

    change model design

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:edu_flutter_dart/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
@@ -5,11 +8,17 @@ class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      print(webtoonInstances.first.title);
+      return webtoonInstances;
     }
     throw Error();
   }
