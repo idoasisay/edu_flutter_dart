b367d7c26cae260d34385612dfe1c83ab030ce35
idoasisay <ihannah@kakao.com>
2024-05-24 22:15:56 +0900

    study memo

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,10 +5,15 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   //궁금점. 그냥 함수를 쓰면 되는데 왜 class를 쓰지?
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  // final -> static const로 변환
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  // final -> static const로 변환
+  static const String today = "today";
+
+  // final -> static const로 변환
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
@@ -17,9 +22,12 @@ class ApiService {
       for (var webtoon in webtoons) {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
-      print(webtoonInstances.first.title);
       return webtoonInstances;
     }
     throw Error();
   }
 }
+
+
+// 공부해야 할 것
+// -> static을 하는 이유는 무엇일까?
