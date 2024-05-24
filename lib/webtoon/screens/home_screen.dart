ec887758fd65c504ca4915b4cdec50a9240a528b
idoasisay <ihannah@kakao.com>
2024-05-24 22:47:37 +0900

    study ListView widget

--- lib/webtoon/screens/home_screen.dart
+++ lib/webtoon/screens/home_screen.dart
@@ -2,16 +2,17 @@ import 'package:edu_flutter_dart/models/webtoon_model.dart';
 import 'package:edu_flutter_dart/services/api_service.dart';
 import 'package:flutter/material.dart';
 
-/// setState는 많이 쓰지 않는 게 좋다 (많이 쓰이지 않는다)
-/// statelessWidget에서 비동기를 쓰는 방법
-///
-/// FutureBuilder
+// setState는 많이 쓰지 않는 게 좋다 (많이 쓰이지 않는다)
+// statelessWidget에서 비동기를 쓰는 방법
+//
+// FutureBuilder
 
-// ignore: must_be_immutable
+// This class (or a class that this class inherits from) is marked as '@immutable',
+// but one or more of its instance fields aren't final:
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +34,17 @@ class HomeScreen extends StatelessWidget {
           // context : buildcontext
           // snapshot : know the statue of Future
           if (snapshot.hasData) {
-            return const Text("There is data!");
+            // 대량의 항목을 나열할 땐 ListView Widget을 사용하는 게 좋다
+            // Column, Row X
+            // 자동 스크롤뷰 탑재
+            return ListView(
+              children: [
+                // snapshot.data가 null이 아님을 알려주기 위해 뒤에 !를 붙임
+                for (var webtoon in snapshot.data!) Text(webtoon.title)
+              ],
+            );
           }
-          return const Text("Loading");
+          return const Center(child: CircularProgressIndicator());
         },
       ),
     );
