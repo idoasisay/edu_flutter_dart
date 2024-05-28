dc09aad741f97e4113004dae107c6a8c111cf595
idoasisay <ihannah@kakao.com>
2024-05-28 18:52:44 +0900

    create Hero widget

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -35,27 +35,30 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            clipBehavior: Clip.hardEdge,
-            decoration:
-                // clipBehavior 때문에 보더가 먹히지 않는다.
-                BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                  // 그림자 위쪽이 잘려 있음
-                  // List에 Padding이 없어서 그렇다
-                  BoxShadow(
-                      blurRadius: 7,
-                      offset: Offset(0, 0),
-                      color: Color.fromARGB(123, 0, 0, 0))
-                ]),
-            width: 250,
-            child: Image.network(
-              thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
+          Hero(
+            tag: id,
+            child: Container(
+              clipBehavior: Clip.hardEdge,
+              decoration:
+                  // clipBehavior 때문에 보더가 먹히지 않는다.
+                  BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                    // 그림자 위쪽이 잘려 있음
+                    // List에 Padding이 없어서 그렇다
+                    BoxShadow(
+                        blurRadius: 7,
+                        offset: Offset(0, 0),
+                        color: Color.fromARGB(123, 0, 0, 0))
+                  ]),
+              width: 250,
+              child: Image.network(
+                thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
             ),
           ),
           const SizedBox(
